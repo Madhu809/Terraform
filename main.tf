cf9e3d79b613fe2813e7fe91e8325a624d96080e
Madhu N <madhun@Swapnas-MacBook-Air.local>
2026-06-27 18:27:42 +0530

    private

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_versioning"  "my_test_bucket_versioning" {
     }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "my_test_bucket_AES256" {
   bucket = aws_s3_bucket.my_test_bucket.id
 
   rule {
